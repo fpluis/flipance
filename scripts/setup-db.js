d3dc8171cb82a6f910d74e63a015d677ee80a462
Luis Fominaya <fnn.fpluis@gmail.com>
2022-05-19 09:43:57 +0200

    Drop db if it exists on the setup-db script

--- scripts/setup-db.js
+++ scripts/setup-db.js
@@ -1,12 +1,23 @@
 /* eslint-disable no-multi-str */
 import path from "path";
 import dotenv from "dotenv";
-import { createDb, setUpDb } from "../src/database/index.js";
+import {
+  createDb,
+  setUpDb,
+  isDbCreated,
+  removeDb,
+} from "../src/database/index.js";
 
 dotenv.config({ path: path.resolve(".env") });
 
-createDb().then(async () => {
-  console.log(`DB created`);
-  await setUpDb();
-  console.log(`DB set up`);
-});
+const setUp = async () => {
+  const dbExists = await isDbCreated();
+  if (dbExists) {
+    await removeDb();
+  }
+
+  await createDb();
+  return setUpDb();
+};
+
+setUp();
