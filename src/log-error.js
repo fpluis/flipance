fe6d880404dcc1e0071c792af2df742f179905e3
Luis Fominaya <fnn.fpluis@gmail.com>
2022-05-21 11:47:58 +0200

    Create log folder on log-error if it doesn't exist

--- src/log-error.js
+++ src/log-error.js
@@ -1,6 +1,11 @@
-import { createWriteStream } from "fs";
+import { existsSync, mkdirSync, createWriteStream } from "fs";
 
-const ERROR_OUTPUT_PATH = "logs/errors.txt";
+const ERROR_OUTPUT_FOLDER = "logs";
+const ERROR_OUTPUT_PATH = `${ERROR_OUTPUT_FOLDER}/errors.txt`;
+
+if (!existsSync(ERROR_OUTPUT_FOLDER)) {
+  mkdirSync(ERROR_OUTPUT_FOLDER);
+}
 
 const errorStream = createWriteStream(ERROR_OUTPUT_PATH, { flags: "a" });
 
